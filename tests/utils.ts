2c4b45a1222a9edc10575488ee2f92984dc258d9
Kris Kuiper <kriskuiper74@gmail.com>
2023-06-25 11:49:23 +0200

    test: add types for testing library

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,7 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
+import { ReactElement } from "react";
+import { render, RenderOptions } from "@testing-library/react";
 
-const customRender = (ui, options) => render(ui, options);
+const customRender = (ui: ReactElement, options: RenderOptions) =>
+  render(ui, options);
 
 export * from "@testing-library/react";
 export { customRender as render };
