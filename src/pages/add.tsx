1718d4f0efbb112732a67b9af6ea52442bb568c2
Kris Kuiper <kriskuiper74@gmail.com>
2023-06-25 12:38:21 +0200

    feat(add page): set up form for adding a password

--- src/pages/add.tsx
+++ src/pages/add.tsx
@@ -1,7 +1,37 @@
+const TITLE_INPUT_NAME = "name";
+const PASSWORD_INPUT_NAME = "password";
+const CLIENT_INPUT_NAME = "client";
+
 function AddPage() {
   return (
     <main>
-      <h1>Hello from le add page</h1>
+      <h1>Add a bloody hot password</h1>
+
+      <form onSubmit={addPassword}>
+        <div>
+          <label htmlFor="name">Title:</label>
+          <input type="text" id="name" name={TITLE_INPUT_NAME} required />
+          <p>Give your password a title so you can identify it later on</p>
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="text" id="password" name="password" required />
+        </div>
+
+        <div>
+          <label htmlFor="client">Client</label>
+          <select name={CLIENT_INPUT_NAME} id="client" defaultValue="" required>
+            <option value="" disabled>
+              Choose a client
+            </option>
+            <option value="one">Option one</option>
+            <option value="two">Option two</option>
+          </select>
+        </div>
+
+        <button>Add</button>
+      </form>
     </main>
   );
 }
